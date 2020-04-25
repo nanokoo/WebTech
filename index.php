4e06a43b3940e4f08299490332ac4838c7cae0eb
nakhan <nakhan@ttu.ee>
2020-04-25 19:14:25 +0300

    added video and css for it

--- index.php
+++ index.php
@@ -8,8 +8,9 @@
         <section class="section-default">
           <?php
           if (!isset($_SESSION['id'])) {
-            echo '<p class="login-status">You are logged out!</p>';
-          }
+            echo     '<video autoplay muted loop class="myvideo" width="100%", height="300">';
+            echo        '<source src="img/movi_WebTech1.mp4" type="video/mp4">';
+            echo      '</video>';
           // here will be added the page what the user will see when he is logged in, currently just for this milestone leaving it like this
           else if (isset($_SESSION['id'])) {
             echo '<p class="login-status">You are logged in!</p>';
