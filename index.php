670a0200938ff16498dadcc9d81937ab189525f7
nakhan <nakhan@ttu.ee>
2020-04-26 15:53:17 +0300

    added fullcalendar view with Jquery, added delete event update event

--- index.php
+++ index.php
@@ -11,9 +11,9 @@
             echo     '<video autoplay muted loop class="myvideo" width="100%", height="300">';
             echo        '<source src="img/movi_WebTech1.mp4" type="video/mp4">';
             echo      '</video>';
-          // here will be added the page what the user will see when he is logged in, currently just for this milestone leaving it like this
+          
           else if (isset($_SESSION['id'])) {
-            echo '<p class="login-status">You are logged in!</p>';
+            include 'calendar/index.php';
           }
           ?>
          
